e87a0276b796fa31e8a4b22f3a0d7189547bf1fa
mohammedbouljihel <m.bouljihel@altados.com>
2023-04-27 15:24:38 +0100

    Menu card added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\DishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(DishRepository $dish): Response
     {
+        $dish = $dish->findAll();
+        $rand_dish = array_rand($dish,2) ;
+        dump($dish[$rand_dish[0]]);
+        
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'dish1' => $dish[$rand_dish[0]],
+            'dish2' => $dish[$rand_dish[1]]
+
         ]);
     }
 }
